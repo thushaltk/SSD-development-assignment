3acd4045c3266b011c17522a4f09f4fc810ca485
Eishan-Dinuka-IT18118896 <eishan2dinuka@gmail.com>
2022-11-16 22:53:14 +0530

    Added Manager add Home

--- src/components/manager_staff/manageradd.jsx
+++ src/components/manager_staff/manageradd.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { CircularProgress } from "@mui/material";
+import { Alert, CircularProgress } from "@mui/material";
 import Lottie from "react-lottie";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
@@ -7,10 +7,21 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import * as animationData from "../../assets/images/manager.json";
+import managerService from "../../services/manager-service";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 const Manageradd = (props) => {
   const [open, setOpen] = useState(props.open);
+  const navigate = useNavigate();
+  const accessToken = useSelector((state) => state.accessToken.value);
   const [isLoading, setIsLoading] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [openAlert, setOpenAlert] = useState(false);
+  const [openSuccessAlert, setOpenSuccessAlert] = useState(false);
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredConfPassword, setEnteredConfPassword] = useState("");
 
   const defaultOptions = {
     loop: true,
@@ -32,6 +43,50 @@ const Manageradd = (props) => {
 
   const register = () => {
     setIsLoading(true);
+    if (enteredPassword !== enteredConfPassword) {
+      setIsLoading(false);
+      setOpenAlert(true);
+      setAlertMessage("Password Mismatched. Check spellings again.");
+      setTimeout(() => {
+        setOpenAlert(false);
+      }, 2000);
+    } else {
+      const formData = {
+        username: enteredUsername,
+        password: enteredPassword,
+      };
+      managerService.addNewManager(accessToken, formData).then((res) => {
+        console.log(res);
+        if (res.data.message === "Failed to authenticate token") {
+          setIsLoading(false);
+          setOpenAlert(true);
+          setAlertMessage("Session Expired. Login again");
+          setTimeout(() => {
+            localStorage.removeItem("isAdminLoggedIn");
+            navigate("/", { replace: true });
+          }, 1000);
+        } else {
+          setIsLoading(false);
+          setOpenSuccessAlert(true);
+          setAlertMessage("Manager Added Successfully!");
+          setTimeout(() => {
+            props.close();
+          }, 2000);
+        }
+      });
+    }
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const confPasswordChangeHandler = (event) => {
+    setEnteredConfPassword(event.target.value);
   };
 
   return (
@@ -47,6 +102,7 @@ const Manageradd = (props) => {
             isPaused={false}
           />
           <TextField
+            onChange={usernameChangeHandler}
             autoFocus
             style={{ width: "550px" }}
             margin="dense"
@@ -56,6 +112,7 @@ const Manageradd = (props) => {
             variant="standard"
           />
           <TextField
+            onChange={passwordChangeHandler}
             autoFocus
             style={{ width: "550px" }}
             margin="dense"
@@ -65,6 +122,7 @@ const Manageradd = (props) => {
             variant="standard"
           />
           <TextField
+            onChange={confPasswordChangeHandler}
             autoFocus
             style={{ width: "550px" }}
             margin="dense"
@@ -84,13 +142,13 @@ const Manageradd = (props) => {
               Close
             </Button>
           </div>
-          <div style={{width: "100px", textAlign: 'center'}}>
+          <div style={{ width: "100px", textAlign: "center" }}>
             {isLoading ? (
               <CircularProgress />
             ) : (
               <Button
                 variant="contained"
-                style={{ backgroundColor: "green", width: "100px"}}
+                style={{ backgroundColor: "green", width: "100px" }}
                 onClick={register}
               >
                 ADD
@@ -98,6 +156,24 @@ const Manageradd = (props) => {
             )}
           </div>
         </DialogActions>
+        <div>
+          {openAlert ? (
+            <Alert variant="filled" severity="error">
+              {alertMessage}
+            </Alert>
+          ) : (
+            ""
+          )}
+        </div>
+        <div>
+          {openSuccessAlert ? (
+            <Alert variant="filled" severity="success">
+              {alertMessage}
+            </Alert>
+          ) : (
+            ""
+          )}
+        </div>
       </Dialog>
     </>
   );
