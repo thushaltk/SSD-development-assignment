74e0da51a82e39293da5db568b40ff46132184b6
Eishan-Dinuka-IT18118896 <eishan2dinuka@gmail.com>
2022-11-16 23:03:00 +0530

    Updated the routing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import LandingPage from "./components/landing_page/landingpage";
 import Adminmain from "./components/admin_staff/adminmain";
 import Managerstaffmain from "./components/manager_staff/managerstaffmain";
 import Workerstaffmain from "./components/worker_staff/workerstaffmain";
+import Layout from "./components/layout";
+import { Route, Routes } from "react-router";
+import RequireAuth from "./components/requireauth";
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Protected routes begins here */}
+        <Route element={<RequireAuth allowedRoles={["admin"]} />}>
+          <Route path="/admin" element={<Adminmain />} />
+        </Route>
+        <Route element={<RequireAuth allowedRoles={["manager"]} />}>
+          <Route path="/manager" element={<Managerstaffmain />} />
+        </Route>
+        <Route element={<RequireAuth allowedRoles={["worker"]} />}>
+          <Route path="/worker" element={<Workerstaffmain />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { store } from "./store";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/authprovider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <AuthProvider>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </AuthProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
