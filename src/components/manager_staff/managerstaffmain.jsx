e9af95be4a4e26917a3b3608b25cbdfc590448bc
Kishen Deemud <deemudk@gmail.com>
2022-11-17 09:38:11 +0530

    Added file upload function

--- src/components/manager_staff/managerstaffmain.jsx
+++ src/components/manager_staff/managerstaffmain.jsx
@@ -9,6 +9,10 @@ import managerService from "../../services/manager-service";
 import { useSelector } from "react-redux";
 import useAuth from "../../hooks/useAuth";
 import { useNavigate } from "react-router";
+import { storage } from "../../utils/firebase";
+import { ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
+import { Logout } from "@mui/icons-material";
 
 const Managerstaffmain = () => {
   const accessToken = useSelector((state) => state.accessToken.value);
@@ -16,21 +20,21 @@ const Managerstaffmain = () => {
   const [enteredMessage, setEnteredMessage] = useState("");
   const [alertMessage, setAlertMessage] = useState("");
   const [openAlert, setOpenAlert] = useState(false);
+  const [openSuccessAlert, setOpenSuccessAlert] = useState(false);
   const navigate = useNavigate();
   const from = "/";
-  const [selectedImage, setSelectedImage] = useState(
-    "https://cdn-icons-png.flaticon.com/512/126/126477.png"
-  );
+  const [selectedFile, setSelectedFile] = useState(null);
 
   useEffect(() => {
     setOpenAlert(false);
+    setOpenSuccessAlert(false);
     if (localStorage.getItem("isManagerLoggedIn") !== "true") {
       navigate(from, { replace: true });
     }
   }, []);
 
-  const selectImageChangeHandler = (data) => {
-    setSelectedImage(data.target.files[0]);
+  const selectFileChangeHandler = (data) => {
+    setSelectedFile(data.target.files[0]);
   };
 
   const onChangeMessageHandler = (event) => {
@@ -38,25 +42,77 @@ const Managerstaffmain = () => {
   };
 
   const onSubmit = () => {
-    managerService
-      .saveMsgs(accessToken, { username: auth.user, message: enteredMessage })
-      .then((res) => {
-        console.log(res);
-        if (res.data.message == "Failed to authenticate token") {
-          setOpenAlert(true);
-          setAlertMessage("Session expired. Login again");
-          setTimeout(() => {
-            localStorage.removeItem("isManagerLoggedIn");
-            localStorage.removeItem("isAdminLoggedIn");
-            localStorage.removeItem("isWorkerLoggedIn");
-            navigate(from, { replace: true });
-          }, 1000);
-        }
+    if (!enteredMessage) {
+      setOpenAlert(true);
+      setAlertMessage("Cannot send empty messages");
+      setTimeout(() => {
+        setOpenAlert(false);
+      }, 2000);
+    } else {
+      managerService
+        .saveMsgs(accessToken, { username: auth.user, message: enteredMessage })
+        .then((res) => {
+          console.log(res);
+          if (res.data.message == "Failed to authenticate token") {
+            setOpenAlert(true);
+            setAlertMessage("Session expired. Login again");
+            setTimeout(() => {
+              localStorage.removeItem("isManagerLoggedIn");
+              localStorage.removeItem("isAdminLoggedIn");
+              localStorage.removeItem("isWorkerLoggedIn");
+              navigate(from, { replace: true });
+            }, 1000);
+          } else {
+            setOpenSuccessAlert(true);
+            setAlertMessage("Message Saved Successfully!");
+            setTimeout(() => {
+              setOpenSuccessAlert(false);
+            }, 2000);
+          }
+        });
+    }
+  };
+
+  const onLogout = () => {
+    localStorage.removeItem("isManagerLoggedIn");
+    localStorage.removeItem("isAdminLoggedIn");
+    localStorage.removeItem("isWorkerLoggedIn");
+    navigate(from, { replace: true });
+  };
+
+  const uploadFile = () => {
+    if (selectedFile === null) {
+      setOpenAlert(true);
+      setAlertMessage("Please choose a file");
+      setTimeout(() => {
+        setOpenAlert(false);
+      }, 2000);
+    } else {
+      const fileRef = ref(storage, `files/${selectedFile.name + v4()}`);
+      uploadBytes(fileRef, selectedFile).then(() => {
+        setOpenSuccessAlert(true);
+        setAlertMessage("Uploaded file Successfully!");
+        setTimeout(() => {
+          setOpenSuccessAlert(false);
+          localStorage.removeItem("isManagerLoggedIn");
+          localStorage.removeItem("isAdminLoggedIn");
+          localStorage.removeItem("isWorkerLoggedIn");
+          navigate(from, { replace: true });
+        }, 2000);
       });
+    }
   };
 
   return (
     <div id="managermain">
+      <Button
+        variant="text"
+        color="primary"
+        endIcon={<Logout />}
+        onClick={onLogout}
+      >
+        Logout
+      </Button>
       <div
         style={{
           display: "flex",
@@ -116,24 +172,16 @@ const Managerstaffmain = () => {
               flexDirection: "column",
             }}
           >
-            <img
-              src={
-                typeof selectedImage == "object"
-                  ? URL.createObjectURL(selectedImage)
-                  : selectedImage
-              }
-              style={{ width: "100px", height: "100px" }}
-            />
             <br />
             <br />
             <input
-              accept="image/*"
+              accept="application/pdf"
               type="file"
-              onChange={selectImageChangeHandler}
+              onChange={selectFileChangeHandler}
             />
             <br />
             <br />
-            <Button variant="contained" component="label">
+            <Button onClick={uploadFile} variant="contained" component="label">
               Upload image
             </Button>
           </div>
@@ -142,6 +190,15 @@ const Managerstaffmain = () => {
       <div>
         {openAlert ? (
           <Alert variant="filled" severity="error">
+            {alertMessage}
+          </Alert>
+        ) : (
+          ""
+        )}
+      </div>
+      <div>
+        {openSuccessAlert ? (
+          <Alert variant="filled" severity="success">
             {alertMessage}
           </Alert>
         ) : (
